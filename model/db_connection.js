69751603119b09d9e9960d8bc31b009ae6a67b48
keegandsouza15 <keegan0415dsouza@gmail.com>
2016-12-12 15:46:54 -0800

    Monday 3:46 pm

---
+++ model/db_connection.js
@@ -0,0 +1,6 @@
+exports.config = {
+        user: 'kdsouza',
+        password: 'keegancs115',
+        host: 'blue.cs.sonoma.edu',
+        database: 'kdsouza'
+};
---
+++ routes/Flavor.js
@@ -0,0 +1,17 @@
+/**
+ * Created by student on 12/12/16.
+ */
+var express = require('express');
+var router = express.Router();
+var Flavor_dal = require('../model/Flavor_dal')
+
+router.get('/all',function (req, res){
+    Flavor_dal.getAll(function (err, result) {
+        if(err){
+            res.send(err);
+        }
+        else{
+            res.render('Flavor/FlavorViewAll',{'result':result})
+        }
+    })
+})

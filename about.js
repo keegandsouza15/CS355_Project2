22ef0686f617aa34fcbbf649febaad020f9de510
keegandsouza15 <keegan0415dsouza@gmail.com>
2016-12-13 18:38:36 -0800

    Add files via upload

---
+++ about.js
@@ -0,0 +1,24 @@
+/**
+ * Created by dell on 12/13/2016.
+ */
+/**
+ * Created by student on 12/12/16.
+ */
+var express = require('express');
+var router = express.Router();
+
+router.get('/',function (req,res){
+    res.render('about');
+
+})
+
+router.get('/er', function (req,res) {
+    res.render('Project 1 ER Diagram');
+
+})
+
+router.get('/rs', function (req,res){
+    res.render('Relational Schema Project 1 - Page 1.ejs');
+})
+
+module.exports = router;
